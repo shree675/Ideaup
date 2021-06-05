82fd6a40f4d1d1759b4e810ae886d3842e8ef125
shree675 <shreeteshm37@gmail.com>
2021-06-05 19:57:51 +0530

    removed materialui

--- src/components/nav-bar/navbar.js
+++ src/components/nav-bar/navbar.js
@@ -1,87 +1,68 @@
-import { Component,useState } from "react";
+import { Component, useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 class Navbar extends Component {
- 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      scrolled: true,
-      y: 100,
-      color: "white",
-    };
-  }
+        this.state = {
+            scrolled: true,
+            y: 100,
+            color: "white",
+        };
+    }
 
-  componentDidMount() {
-    document.addEventListener("scroll", (e) => {
-      let scrolled = document.scrollingElement.scrollTop;
-      if (scrolled >= 50) {
-        this.setState({
-          scrolled: false,
+    componentDidMount() {
+        document.addEventListener("scroll", (e) => {
+            let scrolled = document.scrollingElement.scrollTop;
+            if (scrolled >= 50) {
+                this.setState({
+                    scrolled: false,
+                });
+            } else {
+                this.setState({
+                    scrolled: true,
+                });
+            }
         });
-      } else {
-        this.setState({
-          scrolled: true,
-        });
-      }
-    });
-  }
+    }
 
-  render() {
-    return (
-      <div className="mainwrap">
-      <div className="navbar-main">
-        
-        <div
-          className={
-            this.state.scrolled ? "inside-navbar" : "inside-navbar-scrolled"
-          }
-        >
-          <Link to="/" style={{ textDecoration: `none` }}>
-            <div className={this.state.scrolled ? "ideaup" : "ideaup-scrolled"}>
-              IDEA
-              <span style={{ color: `goldenrod` }}>UP</span>
-            </div>
-          </Link>
-          <div className="right">
-        
-            <div
-              className={
-                this.state.scrolled ? "nav-button" : "nav-button-scrolled"
-              }
-            >
-              <Link to="/" style={{ textDecoration: `none` }}>
-                <span className="but-nav">About Us</span>
-              </Link>
-            </div>
+    render() {
+        return (
+            <div className='mainwrap'>
+                <div className='navbar-main'>
+                    <div className={this.state.scrolled ? "inside-navbar" : "inside-navbar-scrolled"}>
+                        <Link to='/' style={{ textDecoration: `none` }}>
+                            <div className={this.state.scrolled ? "ideaup" : "ideaup-scrolled"}>
+                                IDEA
+                                <span style={{ color: `goldenrod` }}>UP</span>
+                            </div>
+                        </Link>
+                        <div className='right'>
+                            <div className={this.state.scrolled ? "nav-button" : "nav-button-scrolled"}>
+                                <Link to='/' style={{ textDecoration: `none` }}>
+                                    <span className='but-nav'>About Us</span>
+                                </Link>
+                            </div>
 
-            <div
-              className={
-                this.state.scrolled ? "nav-button" : "nav-button-scrolled"
-              }
-            >
-              <Link to="/" style={{ textDecoration: `none` }}>
-                <span className="but-nav">Associates</span>
-              </Link>
-            </div>
+                            <div className={this.state.scrolled ? "nav-button" : "nav-button-scrolled"}>
+                                <Link to='/' style={{ textDecoration: `none` }}>
+                                    <span className='but-nav'>Associates</span>
+                                </Link>
+                            </div>
 
-            <div
-              className={
-                this.state.scrolled ? "nav-button" : "nav-button-scrolled"
-              }
-            >
-              <Link to="/" style={{ textDecoration: `none` }}>
-                <span className="but-nav">Our Products</span>
-              </Link>
+                            <div className={this.state.scrolled ? "nav-button" : "nav-button-scrolled"}>
+                                <Link to='/' style={{ textDecoration: `none` }}>
+                                    <span className='but-nav'>Our Products</span>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-      </div>
-      );
-  }
+        );
+    }
 }
 
 export default Navbar;
